499f15f7ece4272ee80ff9b05f1797adfba795b6
dan1eL27 <105814074+dan1eL27@users.noreply.github.com>
2023-04-21 17:03:09 +0200

    edit config

--- vizsgaremek/test/config.py
+++ vizsgaremek/test/config.py
@@ -4,13 +4,12 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def chrome_driver_config():
-    s = Service(executable_path=ChromeDriverManager().install())
-    o = Options()
-    o.add_experimental_option('detach', True)
-    browser = webdriver.Chrome(service=s, options=o)
-    o.add_argument('--headless')
-    o.add_argument('--no-sandbox')
-    o.add_argument('--disable-dev-shm-usage')
-
-    return browser
+def chrome_driver_config(self, remote=True):
+    def __init__(self, remote=True):
+        options = Options()
+        options.add_experimental_option('detach', True)
+        if remote:
+            options.add_argument('--headless')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver(service=Service(executable_path=ChromeDriverManager().install()), options=options)
